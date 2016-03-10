6dcbd9376fd4ea8cc778b5dec2bc8bf27dab2cd5
John K. Parejko <parejkoj@uw.edu>
2016-03-10 16:11:38 -0700

    Add eupspkg.cfg.sh for Darwin building.

---
+++ ups/eupspkg.cfg.sh
@@ -0,0 +1,9 @@
+TAP_PACKAGE=1
+
+config()
+{
+    # use the correct config file when building on OSX
+    if [[ "$(uname)" == "Darwin" ]]; then
+        mv SuiteSparse_config/SuiteSparse_config_Mac.mk SuiteSparse_config/SuiteSparse_config.mk
+    fi
+}
